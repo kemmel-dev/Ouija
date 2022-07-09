dc06af33d434170a5b187d6da169d7d744ad8d4e
Kamiel <kamieldevisser@gmail.com>
2022-07-09 19:07:55 +0200

    add rooms

--- sketch.js
+++ sketch.js
@@ -1,47 +1,33 @@
-// Import the functions you need from the SDKs you need
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
-import { getDatabase, ref, set, increment } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-database.js";
+import { getDatabase, get, ref, child, set, push} from "https://www.gstatic.com/firebasejs/9.8.4/firebase-database.js";
+
 import { draw as drawTitle, setup as setupTitle } from "./titlescreen.js"
 import { draw as drawPlay, setup as setupPlay } from "./play.js"
+import { draw as drawHost, setup as setupHost } from "./host.js"
 
-let app, database;
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  };
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 const Scenes = {
     Title: 0,
-    Play: 1
+    Play: 1,
+    Host: 2
 }
 
 var currentScene = Scenes.Title;
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-
-  apiKey: "AIzaSyALXOuUs1htkIUVRzl6-tTSsJZNpZzuSdw",
-
-  authDomain: "ouija-273ba.firebaseapp.com",
-
-  databaseURL: "https://ouija-273ba-default-rtdb.europe-west1.firebasedatabase.app",
-
-  projectId: "ouija-273ba",
-
-  storageBucket: "ouija-273ba.appspot.com",
-
-  messagingSenderId: "501172041925",
-
-  appId: "1:501172041925:web:68fad9bce231d7ba1e98ac"
-
-};
-
-window.setup = function () {
+window.setup = function() 
+{
     let canvas = createCanvas(400,400);
     canvas.parent("sketch-holder");
     canvas.id("sketch");
-
     textAlign(CENTER);
 
     setupScenes();
-    initFirebase();
 }
 
 window.draw = function () {
@@ -53,6 +39,9 @@ window.draw = function () {
         case Scenes.Play:
             drawPlay();
             break;
+        case Scenes.Host:
+            drawHost();
+            break;
         break;
     }
 }
@@ -60,26 +49,27 @@ window.draw = function () {
 function setupScenes()
 {
     setupTitle();
+    setupHost();
     setupPlay();
 }
 
-function initFirebase()
+async function checkRoomCode(roomCode)
 {
-    // Initialize Firebase
-    app = initializeApp(firebaseConfig);
-    database = getDatabase(app);
+    return get(child(ref(database), `rooms/${roomCode}`)).then((snapshot) => {
+        console.log("in checkroomcode");
+        return snapshot.exists();
+      }).catch((error) => {
+        console.error(error);
+        return null;
+      });
 }
 
-function pushdb(roomCode)
+const newRoom = {A: 0, B: 0, C: 0, D: 0, E: 0, F: 0, G: 0, H: 0, I: 0, J: 0, K: 0, L: 0, M: 0, N: 0, O: 0, P: 0, Q: 0, R: 0, S: 0, T: 0, U: 0, V: 0, W: 0, X: 0, Y: 0, Z: 0}
+
+function createRoom(roomCode)
 {
-    set(ref(database, roomCode), { a: 1});
+    set(ref(database, `rooms/${roomCode}`), newRoom);
 }
 
-const incrementer = async (roomcode) => {
-    await set(ref(database, roomcode), {
-        b: increment(1)
-    });
-    currentScene = Scenes.Play;
-  } 
 
-export { pushdb, incrementer }
+export { checkRoomCode, createRoom }
--- titlescreen.js
+++ titlescreen.js
@@ -1,6 +1,6 @@
-import { pushdb, incrementer } from "./sketch.js"
+import {checkRoomCode, createRoom} from "./sketch.js"
 
-let roomcodeField, joinButton;
+let roomcodeField, joinButton, hostButton;
 
 function setup()
 {
@@ -14,8 +14,15 @@ function setup()
     joinButton.parent("sketch-holder");
     joinButton.center();
     joinButton.position(joinButton.position().x, joinButton.position().y + width / 8)
-
     joinButton.mousePressed(join);
+
+
+    hostButton = createButton("Host!");
+    hostButton.parent("sketch-holder");
+    hostButton.center();
+    hostButton.position(hostButton.position().x, hostButton.position().y - width / 6)
+    hostButton.mousePressed(host);
+
 }
 
 function draw()
@@ -31,6 +38,25 @@ function join()
     incrementer(roomcodeField.value());
     roomcodeField.hide();
     joinButton.hide();
+}
+
+async function host()
+{
+    var exists = false, roomCode;
+    for(var i = 0; i < 5; i++)
+    {
+        roomCode = Math.random().toString(36).slice(2, 7);
+        if (await checkRoomCode(roomCode))
+        {
+            exists = true;
+            break;
+        }
+    }
+    
+    if (!exists)
+    {
+        createRoom(roomCode);
+    }
 }
 
 export { setup, draw } 
